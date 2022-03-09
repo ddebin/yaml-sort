5e28962a565b18873a6d39775e90fe7ae973e59f
Damien Debin <damien.debin@gmail.com>
2022-03-09 15:53:12 +0100

    Dependency update Use standardJS instead of eslint

--- test/test.js
+++ test/test.js
@@ -1,85 +1,93 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const test = require('tape');
-const spawn = require('tape-spawn');
-const opts = {cwd: __dirname};
+const test = require('tape')
+const spawn = require('tape-spawn')
+const opts = { cwd: __dirname }
 
 test('CLI w/o arg', (t) => {
-    const proc = spawn(t, '../yaml-sort.js', opts);
-    proc.exitCode(1);
-    proc.stderr.match(/Missing required argument: input/);
-    proc.end();
-});
+  const proc = spawn(t, '../yaml-sort.js', opts)
+  proc.exitCode(1)
+  proc.stderr.match(/Missing required argument: input/)
+  proc.end()
+})
 
 test('CLI w/ arg', (t) => {
-    const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout', opts);
-    proc.exitCode(0);
-    proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
+  const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout', opts)
+  proc.exitCode(0)
+  proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
         'b:\n' +
         '  b: 35\n' +
         '  c:\n' +
         '    d: false\n' +
-        '\n');
-    proc.end();
-});
+        '\n')
+  proc.end()
+})
 
 test('CLI --output', (t) => {
-    const proc = spawn(t,
-        '../yaml-sort.js --input test.yml --output output.yml' +
+  const proc = spawn(t,
+    '../yaml-sort.js --input test.yml --output output.yml' +
         ' && cat output.yml' +
-        ' && rm -f output.yml', opts);
-    proc.exitCode(0);
-    proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
+        ' && rm -f output.yml', opts)
+  proc.exitCode(0)
+  proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
         'b:\n' +
         '  b: 35\n' +
         '  c:\n' +
-        '    d: false\n');
-    proc.end();
-});
+        '    d: false\n')
+  proc.end()
+})
 
 test('CLI --indent', (t) => {
-    const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout --indent 4', opts);
-    proc.exitCode(0);
-    proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
+  const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout --indent 4', opts)
+  proc.exitCode(0)
+  proc.stdout.match('a: Lorem ipsum dolor sit amet, consectetur adipiscing elit...\n' +
         'b:\n' +
         '    b: 35\n' +
         '    c:\n' +
         '        d: false\n' +
-        '\n');
-    proc.end();
-});
+        '\n')
+  proc.end()
+})
 
 test('CLI --lineWidth', (t) => {
-    const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout --lineWidth 40', opts);
-    proc.exitCode(0);
-    proc.stdout.match('a: >-\n' +
+  const proc = spawn(t, '../yaml-sort.js --input test.yml --stdout --lineWidth 40', opts)
+  proc.exitCode(0)
+  proc.stdout.match('a: >-\n' +
         '  Lorem ipsum dolor sit amet, consectetur\n' +
         '  adipiscing elit...\n' +
         'b:\n' +
         '  b: 35\n' +
         '  c:\n' +
         '    d: false\n' +
-        '\n');
-    proc.end();
-});
+        '\n')
+  proc.end()
+})
 
 test('CLI --check FAIL', (t) => {
-    const proc = spawn(t,
-        '../yaml-sort.js --input test.yml --check', opts);
-    proc.exitCode(1);
-    proc.stdout.match('');
-    proc.stderr.match('\'test.yml\' is not sorted and/or formatted (indent, line width).\n');
-    proc.end();
-});
+  const proc = spawn(t,
+    '../yaml-sort.js --input test.yml --check', opts)
+  proc.exitCode(1)
+  proc.stdout.match('')
+  proc.stderr.match('\'test.yml\' is not sorted and/or formatted (indent, line width).\n')
+  proc.end()
+})
+
+test('CLI --check SUCCESS', (t) => {
+  const proc = spawn(t,
+    '../yaml-sort.js --input sorted.yml --check', opts)
+  proc.exitCode(0)
+  proc.stdout.match('')
+  proc.end()
+})
 
 test('CLI --lineWidth SUCCESS', (t) => {
-    const proc = spawn(t,
-        '../yaml-sort.js --input test.yml --output output.yml' +
+  const proc = spawn(t,
+    '../yaml-sort.js --input test.yml --output output.yml' +
         ' && ../yaml-sort.js --input output.yml --check' +
-        ' && rm -f output.yml', opts);
-    proc.exitCode(0);
-    proc.stdout.match('');
-    proc.end();
-});
+        ' && rm -f output.yml', opts)
+  proc.exitCode(0)
+  proc.stdout.match('')
+  proc.end()
+})
--- yaml-sort.js
+++ yaml-sort.js
@@ -1,102 +1,102 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
 const argv = require('yargs')
-    .usage('Usage: $0 [options]')
-    .example(
-        '$0 --input config.yml',
-        'Sorts alphabetically and overwrites the file config.yml.',
-    )
-    .example(
-        '$0 --input config.yml --lineWidth 100 --stdout',
-        'Sorts the file config.yml and output result to STDOUT wrapped to 100 columns',
-    )
-    .example(
-        '$0 --input config.yml --indent 4 --output sorted.yml',
-        'Indents with 4 spaces and outputs result to file sorted.yml.',
-    )
-    .option('input', {
-        alias: 'i',
-        describe: 'The YAML file(s) which needs to be sorted',
-        demandOption: 'Please provide an input file',
-        string: true,
-        array: true,
-    })
-    .option('output', {
-        alias: 'o',
-        describe: 'The YAML file to output sorted content to',
-        string: true,
-    })
-    .option('stdout', {
-        alias: 's',
-        default: false,
-        describe: 'Output the proposed sort to STDOUT only',
-        boolean: true,
-    })
-    .option('check', {
-        alias: 'k',
-        default: false,
-        describe: 'Check if the given file(s) is already sorted',
-        boolean: true,
-    })
-    .option('indent', {
-        alias: 'id',
-        default: 2,
-        describe: 'Indentation width to use (in spaces)',
-        number: true,
-    })
-    .option('lineWidth', {
-        alias: 'w',
-        default: 80,
-        describe: 'Wrap line width',
-        number: true,
-    })
-    .demandOption(['input'])
-    .help('h')
-    .alias('h', 'help')
-    .version()
-    .wrap(null)
-    .argv;
+  .usage('Usage: $0 [options]')
+  .example(
+    '$0 --input config.yml',
+    'Sorts alphabetically and overwrites the file config.yml'
+  )
+  .example(
+    '$0 --input config.yml --lineWidth 100 --stdout',
+    'Sorts the file config.yml and output result to STDOUT wrapped to 100 columns'
+  )
+  .example(
+    '$0 --input config.yml --indent 4 --output sorted.yml',
+    'Indents with 4 spaces and outputs result to file sorted.yml'
+  )
+  .option('input', {
+    alias: 'i',
+    describe: 'The YAML file(s) which needs to be sorted',
+    demandOption: 'Please provide an input file',
+    string: true,
+    array: true
+  })
+  .option('output', {
+    alias: 'o',
+    describe: 'The YAML file to output sorted content to',
+    string: true
+  })
+  .option('stdout', {
+    alias: 's',
+    default: false,
+    describe: 'Output the proposed sort to STDOUT only',
+    boolean: true
+  })
+  .option('check', {
+    alias: 'k',
+    default: false,
+    describe: 'Check if the given file(s) is already sorted',
+    boolean: true
+  })
+  .option('indent', {
+    alias: 'id',
+    default: 2,
+    describe: 'Indentation width to use (in spaces)',
+    number: true
+  })
+  .option('lineWidth', {
+    alias: 'w',
+    default: 80,
+    describe: 'Wrap line width',
+    number: true
+  })
+  .demandOption(['input'])
+  .help('h')
+  .alias('h', 'help')
+  .version()
+  .wrap(null)
+  .argv
 
-const yaml = require('js-yaml');
-const fs = require('fs');
+const yaml = require('js-yaml')
+const fs = require('fs')
 
-let success = true;
+let success = true
 
 argv.input.forEach((file) => {
-    try {
-        const content = fs.readFileSync(file, 'utf8');
+  try {
+    const content = fs.readFileSync(file, 'utf8')
 
-        const sorted = yaml.dump(yaml.load(content), {
-            sortKeys: true,
-            indent: argv.indent,
-            lineWidth: argv.lineWidth,
-        });
+    const sorted = yaml.dump(yaml.load(content), {
+      sortKeys: true,
+      indent: argv.indent,
+      lineWidth: argv.lineWidth
+    })
 
-        if (argv.check) {
-            if (sorted !== content) {
-                success = false;
-                console.warn(`'${file}' is not sorted and/or formatted (indent, line width).`);
-            }
-        } else if (argv.stdout) {
-            console.log(sorted);
-        } else {
-            fs.writeFile(
-                argv.output ? argv.output : file,
-                sorted,
-                (error) => {
-                    if (error) {
-                        return console.error(error);
-                    }
-                });
-        }
-    } catch (error) {
-        success = false;
-        console.error(error);
+    if (argv.check) {
+      if (sorted !== content.toString()) {
+        success = false
+        console.warn(`'${file}' is not sorted and/or formatted (indent, line width).`)
+      }
+    } else if (argv.stdout) {
+      console.log(sorted)
+    } else {
+      fs.writeFile(
+        argv.output ? argv.output : file,
+        sorted,
+        (error) => {
+          if (error) {
+            return console.error(error)
+          }
+        })
     }
-});
+  } catch (error) {
+    success = false
+    console.error(error)
+  }
+})
 
 if (!success) {
-    process.exit(1);
+  process.exit(1)
 }
